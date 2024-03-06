1f2e80cafd0a2806b7a7cfe8f2bfb3fc5e778a7c
eneasdev5 <eneasdev@gmail.com>
2024-03-05 23:06:40 -0300

    aula 17 ddl.sql

--- aula15/gestor-pessoas/modulo_mysql/ddl.sql
+++ aula15/gestor-pessoas/modulo_mysql/ddl.sql
@@ -3,25 +3,26 @@ CREATE  database crud;
 use crud;
 
 -- table de usuario
-CREATE TABLE `usuario` (
-  id INT(5) NOT NULL AUTO_INCREMENT,
-  nome VARCHAR(50) NOT NULL,
-  email VARCHAR(50) unique NOT NULL,
-  login VARCHAR(50) unique NOT NULL,
-  senha VARCHAR(255) not null,
+CREATE TABLE `usuarios` (
+  `id` INT(5) NOT NULL AUTO_INCREMENT,
+  `nome` VARCHAR(50) NOT NULL,
+  `email` VARCHAR(50) unique NOT NULL,
+  `nickname` VARCHAR(50) unique NOT NULL,
+  `senha` VARCHAR(255) not null,
   PRIMARY KEY (`id`)
 );
 
 -- tabela de telefone
-CREATE TABLE `telefone` (
-	id int not null auto_increment primary key,
-	numero_telefone VARCHAR(15) NOT NULL,
-	usuario_id int not null,
-	foreign key(usuario_id) references usuario(id) on delete cascade on update cascade
+CREATE TABLE `telefones` (
+	`id` int not null auto_increment primary key,
+	`numero_telefone` VARCHAR(15) NOT NULL,
+	`usuario_id` int not null,
+	foreign key(`usuario_id`) references `usuarios`(`id`)
+    on delete cascade on update cascade
 );
 
 -- tabela de endereco faz o relacionamento com tabela de usuarios
-CREATE TABLE `endereco` (
+CREATE TABLE `enderecos` (
 	`id` int not null auto_increment primary key,
 	`cep` VARCHAR(15) NOT NULL,
 	`rua` VARCHAR(50) NOT NULL,
@@ -31,5 +32,6 @@ CREATE TABLE `endereco` (
 	`cidade` VARCHAR(50) NOT NULL,
 	`estado` VARCHAR(50) NOT NULL,
 	`usuario_id` int not null,
-	foreign key(usuario_id) references usuario(id) on delete cascade on update cascade
+	foreign key(`usuario_id`) references `usuarios`(`id`)
+    on delete cascade on update cascade
 );
